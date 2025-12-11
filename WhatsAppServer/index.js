d48b041473fd842a672929eed6d52fbfc691ba38
Barbarik0711 <vishalchavan0711@gmail.com>
2025-12-11 21:57:02 +0530

    yws

--- WhatsAppServer/index.js
+++ WhatsAppServer/index.js
@@ -1,25 +1,36 @@
 const express = require("express");
 var http = require("http");
-const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 var server = http.createServer(app);
-var io = require("socket.io")(server,{
-    cors:{
-        origin:"*"
-    }
-});
+var io = require("socket.io")(server);
+var clients = {};
 
-
-//middleWare 
+//middleWare
 app.use(express.json());
-app.use(cors());
+//CORS
+const cors = require('cors'); // <--- ADD THIS
+app.use(cors());   
 
-io.on('connection', (socket) => {
-  console.log('someone connected!');
+io.on("connection", (socket) => {
+  console.log("someone connected!");
+  console.log(socket.id, "has joined");
+  socket.on("signin", (id) => {
+    console.log(id);
+    clients[id] = socket;
+    console.log(clients);
+  });
+  socket.on("message", (msg) => {
+    console.log(msg);
+    let targetId=msg.targetId;
+    console.log(targetId);
+    console.log(clients[targetId]);
+    if(clients[targetId]){
+    clients[targetId].emit("message",msg);
+    }
+  });
 });
 
-
-server.listen(port,()=>{
-    console.log('server started');
+server.listen(port, "0.0.0.0", () => {
+  console.log("server started");
 });
